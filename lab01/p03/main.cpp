627d9f496f59f15fedd3dd4e4892a8163f7d3813
Esenbek Burkanov <burkanov_e@auca.kg>
2021-09-27 08:05:12 +0200

    lab01 p02 completed

---
+++ lab01/p03/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+    string name;
+    cin >> name;
+
+    int grade;
+    cin >> grade;
+
+    cout << name << " " << grade << "\n";
+}
