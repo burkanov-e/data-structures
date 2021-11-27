f4f0963f10c241f32660c2d9e997e80d5d44ca52
Esenbek Burkanov <burkanov_e@auca.kg>
2021-11-27 19:09:36 +0100

    project01 3rd commit

--- project01/BigInt/BigInt.hpp
+++ project01/BigInt/BigInt.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <cctype>
 #include <iosfwd>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 class BigInt {
@@ -14,6 +17,10 @@ class BigInt {
     BigInt() : mIsNegative(false) { mDigits.push_back(0); }
 
     BigInt(const std::string &value) : mIsNegative(false) {
+        if (value.empty()) {
+            throw std::runtime_error("BigInt: empty string");
+        }
+
         size_t i = 0;
 
         if (value[i] == '-' || value[i] == '+') {
@@ -21,14 +28,21 @@ class BigInt {
             i++;
         }
 
-        for (; i != value.size(); i++) {
+        while (i < value.size()) {
             if (!isdigit(value[i])) {
                 throw std::runtime_error(
                     "BigInt: incorrect string initializer");
             }
             mDigits.push_back(value[i] - '0');
+            i++;
+        }
+
+        if (mDigits.empty()) {
+            throw std::runtime_error("BigInt: incorrect string");
         }
     }
+
+    BigInt(long long x) : BigInt(std::to_string(x)) {}
 };
 
 inline std::ostream &operator<<(std::ostream &out, const BigInt &x) {
@@ -42,3 +56,5 @@ inline std::ostream &operator<<(std::ostream &out, const BigInt &x) {
 
     return out;
 }
+
+// static BigInt addAbsValues(const BigInt &a, const BigInt &b) { BigInt r; }
--- project01/BigInt/main.cpp
+++ project01/BigInt/main.cpp
@@ -1,4 +1,5 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <limits>
 #include <sstream>
 
 #include "../../doctest/doctest.h"
@@ -7,38 +8,62 @@
 using namespace std;
 
 TEST_CASE("Default constructor") {
-    BigInt a;
+    BigInt x;
 
     ostringstream sout;
-    sout << a;
+    sout << x;
 
     REQUIRE(sout.str() == "0");
 }
 
-TEST_CASE("Default constructor") {
+TEST_CASE("Constructor with string") {
     ostringstream sout;
 
-    SUBCASE("Case 1: 555") {
-        BigInt a("123");
+    SUBCASE("Case 1: 123") {
+        BigInt x("123");
 
-        sout << a;
+        sout << x;
 
         REQUIRE(sout.str() == "123");
     }
 
     SUBCASE("Case 2: -1234567890") {
-        BigInt a("-1234567890");
+        BigInt x("-1234567890");
 
-        sout << a;
+        sout << x;
 
         REQUIRE(sout.str() == "-1234567890");
     }
 
-    SUBCASE("Case 3: 4824728472048727428978429") {
-        BigInt a("4824728472048727428978429");
+    SUBCASE("Case 3: +123") {
+        BigInt x("+123");
+        sout << x;
 
-        sout << a;
+        REQUIRE(sout.str() == "123");
+    }
+
+    SUBCASE("Case 4: 4824728472048727428978429") {
+        BigInt x("4824728472048727428978429");
+
+        sout << x;
 
         REQUIRE(sout.str() == "4824728472048727428978429");
+    }
+
+    SUBCASE("Case 5: empty string") {
+        REQUIRE_THROWS_AS(BigInt(""), runtime_error);
+    }
+
+    SUBCASE("Case 6: -") { REQUIRE_THROWS_AS(BigInt("-"), runtime_error); }
+
+    SUBCASE("Case 7: +") { REQUIRE_THROWS_AS(BigInt("+"), runtime_error); }
+}
+
+TEST_CASE("BigInt with long long") {
+    ostringstream sout;
+    SUBCASE("-9228372036854775808") {
+        BigInt x(numeric_limits<long long>::min());
+        sout << x;
+        REQUIRE(sout.str() == "-9228372036854775808");
     }
 }
