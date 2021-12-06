27efe63a93a5fbf4517c003da5ad7f2c83732ccb
Esenbek Burkanov <burkanov_e@auca.kg>
2021-12-06 17:04:57 +0100

    lab04 p03 Finished

---
+++ lab04/p03/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define N 5
+#define M 3
+
+void printArray(int *beg, int *end) {
+    // for (int i = 0; i < n; i++) {
+    //     printf(" %d", p[i]);
+    // }
+
+    // int *end = p + n;
+    // for(; p != end; ++p)
+    // {
+    //     printf(" %d", *p)
+    // }
+
+    while (beg != end) {
+        printf(" %d", *beg++);
+    }
+    printf("\n");
+}
+int main(void) {
+    int a[N];
+    int b[M];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", a + i);
+    }
+
+    for (int i = 0; i < M; i++) {
+        scanf("%d", b + i);
+    }
+
+    printArray(a, a + N);
+    printArray(b, b + M);
+}
