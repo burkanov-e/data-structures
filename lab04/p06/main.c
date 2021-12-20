87fd5f6d401999579040ec6bf830e20ccb021b09
Esenbek Burkanov <burkanov_e@auca.kg>
2021-12-20 21:17:56 +0100

    Lab04 p06 Finished

---
+++ lab04/p06/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+#include "VecInt.h"
+
+void printArray(int *beg, int *end) {
+    while (beg != end) {
+        printf(" %d", *beg++);
+    }
+    printf("\n");
+}
+
+void reverse(int *beg, int *end) {
+    for (;;) {
+        if (beg == end) {
+            break;
+        }
+        end--;
+
+        if (beg == end) {
+            break;
+        }
+
+        int temp = *beg;
+        *beg = *end;
+        *end = temp;
+
+        beg++;
+    }
+}
+
+void p01() {
+    printf("the size of array: ");
+
+    int n;
+    scanf("%d", &n);
+
+    struct VecInt v;
+    VecInt_createOfSize(&v, n, 0);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v.data[i]);
+    }
+
+    reverse(v.data, v.data + n);
+
+    printArray(v.data, v.data + n);
+
+    VecInt_destroy(&v);
+}
+
+void p02() {
+    struct VecInt v;
+    VecInt_createEmpty(&v);
+
+    int x;
+    while (scanf("%d", &x) == 1) {
+        VecInt_pushBack(&v, x);
+    }
+
+    printArray(v.data, v.data + v.sz);
+    reverse(v.data, v.data + v.sz);
+    printArray(v.data, v.data + v.sz);
+
+    // VecInt_destroy(&v);
+}
+
+int main(void) { p01(); }
