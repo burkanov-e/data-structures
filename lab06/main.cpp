e8e6d814a0303a2ec844b848ffb68cba1a47862c
Esenbek Burkanov <burkanov_e@auca.kg>
2021-12-23 14:19:37 +0100

    Lab06 p12 Finished

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -404,6 +404,16 @@ void p11() {
     }
 }
 
+void p12() {
+    vector<int> v = {0, 0, 4, 5, 10, 10, 10, 12, 20, 25, 35, 25, 25, 40};
+
+    for (int x; cin >> x;) {
+        auto it = equal_range(begin(v), end(v), x);
+        cout << "number of occurences of " << x << " is "
+             << it.second - it.first << '\n';
+    }
+}
+
 int main() {
     // p0101();
     // p0102();
@@ -427,4 +437,6 @@ int main() {
     // p1002();
 
     // p11();
+
+    p12();
 }
