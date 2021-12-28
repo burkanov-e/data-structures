f4e6020eae92b3c16dd8ec47ef67f6224a8cdd95
Esenbek Burkanov <burkanov_e@auca.kg>
2021-12-28 14:23:42 +0100

    Project02 p13 Wrong answer

---
+++ project02/p13/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) {
+    return static_cast<int>(c.size());
+}
+
+using namespace std;
+
+void printAns(vector<string> words, int width) {
+    sort(begin(words), end(words), [](const string &w1, const string &w2) {
+        string word1 = w1;
+        string word2 = w2;
+
+        reverse(begin(word1), end(word1));
+        reverse(begin(word2), end(word2));
+
+        return word1 < word2;
+    });
+
+    for (auto s : words) {
+        cout << setw(width) << s << '\n';
+    }
+}
+
+int main() {
+    iostream::sync_with_stdio(false);
+
+    string w;
+    vector<string> words;
+
+    int width = 0;
+
+    while (getline(cin, w)) {
+        if (sz(w) > width) {
+            width = sz(w);
+        }
+
+        if (w == "") {
+            printAns(words, width);
+            cout << '\n';
+
+            words.clear();
+            continue;
+        }
+        words.push_back(w);
+    }
+    printAns(words, width);
+}
